5592270be9604017547d4cbb5e938a89dce3db02
danaesm <solrzanodana@gmail.com>
2024-01-05 18:33:09 -0600

    API para users

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # README
+ 
+## Instrucciones para iniciar el proyecto
+
+* Clonar el repositorio.
+* Bajar los archivos.
+* Correr el comando npm run start
+* El proyecto estará corriendo en el puerto 3000, por lo que las URL de la API tendrán este puerto.
+
+## URLs
+
+### Usuarios
+* http://localhost:3000/auth/login
+  * Iniciar sesión
+  * POST
+    * Recibe email y contraseña
+* http://localhost:3000/auth/register
+  * Registrar
+  * POST
+    * Recibe email, nombre y contraseña
+* http://localhost:3000/books
+  * GET
+  * Muestra los libros existentes
+* http://localhost:3000/books/id
+  * GET
+  * Muestra el libro con ese id
+* http://localhost:3000/books
+  * POST
+  * Insertar un libro
+  * recibe todos los campos del libro, excepto el id o un json con la sig. estructura:
+  
+    ```json
+{
+"nombre": "Ejemplo",
+"edad": 25,
+"ubicacion": {
+"ciudad": "Ciudad de Ejemplo",
+"pais": "País de Ejemplo"
+},
+"intereses": ["Programación", "Viajes", "Deportes"]
+}
+```
+
+* http://localhost:3000/books/id
+  * DELETE
+  * Eliminar el libro con ese id
+* http://localhost:3000/books/id
+  * PUT
+  * Actualizar el libro con ese id
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mysql from "mysql2/promise";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const connection = await mysql.createConnection({
     host: 'localhost',
@@ -13,6 +15,66 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hola mundo :)");
+});
+
+app.post("/auth/register", async (req, res) => {
+    const body = req.body;
+
+    try {
+        if (body.email && body.name && body.password) {
+            const hashedPassword = await bcrypt.hash(body.password, 10);
+
+            const [result] = await connection.execute(
+                'INSERT INTO users (email, name, password) VALUES (?, ?, ?)',
+                [body.email, body.name, hashedPassword]
+            );
+
+            const newUserId = result.insertId;
+
+            res.json({ message: "Usuario registrado exitosamente", userId: newUserId });
+        } else {
+            res.status(400).json({ message: "No se completaron los campos necesarios." });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error al registrar al usuario." });
+    }
+});
+
+
+
+
+const JWT_SECRET = 'ClAdaHAeFd6ta$2023&!%hgastFah7Dfafja(886$#';
+
+
+app.post("/auth/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const [rows] = await connection.execute(
+            'SELECT * FROM users WHERE email = ?',
+            [email]
+        );
+
+        if (rows.length > 0) {
+            const user = rows[0];
+
+            const passwordMatch = await bcrypt.compare(password, user.password);
+
+            if (passwordMatch) {
+                const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET, { expiresIn: '24h' });
+
+                res.json({ message: "Inicio de sesión exitoso", token });
+            } else {
+                res.status(401).json({ message: "La contraseña no coincide" });
+            }
+        } else {
+            res.status(404).json({ message: "Usuario no encontrado" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error al iniciar sesión." });
+    }
 });
 
 app.get("/books", async (req, res) => {
